1d5f5285bbbd5f5063d5602cd6d282ac18c245cc
CarinaRoberta <silva.carina@live.com>
2022-04-24 15:27:45 -0300

    created menu mobile

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
+
+  .menuMobile {
+    padding: 0 3rem;
+    position: absolute;
+    width: 100%;
+  }
 `;
 
 const Content = styled.div`
@@ -12,6 +18,15 @@ const Content = styled.div`
   justify-content: space-between;
   margin: 0 auto;
 
+  
+  @media (max-width: 1480px) {
+    padding: 2rem 10rem;
+  }
+
+  @media (max-width: 980px) {
+    padding: 2rem 3rem;
+  }
+
     a {
     padding: 0 1rem;
     font-weight: 700;
@@ -21,6 +36,13 @@ const Content = styled.div`
     &:hover {
         color: var(--dark-blue);)
     }
+  }
+
+  .menuDesktop {
+    
+      @media (max-width:768px) {
+       display: none;
+  }
   }
 `;
 
@@ -45,4 +67,44 @@ const Navigation = styled.nav`
   }
 `;
 
-export { Container, Content, Navigation };
+const MenuMobile = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
+const Dropdown = styled.div`
+  background-color: var(--dark-violet);
+  width: 100%;
+  border-radius: 0.5rem;
+  padding: 1.5rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  z-index: 1;
+
+  a {
+    color: white;
+    padding: 0.5rem;
+    transition: all 0.3s ease;
+
+    &:hover {
+      color: var(--gray);
+    }
+
+    &:nth-child(4) {
+      border-top: 1px solid var(--gray);
+      width: 100%;
+      text-align: center;
+    }
+  }
+`;
+
+export { Container, Content, Navigation, MenuMobile, Dropdown };
